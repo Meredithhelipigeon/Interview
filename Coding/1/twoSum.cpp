1728470b2f1031bff0c8ee4722cc7b5015bb77ae
Meredithhelipigeon <meredithxinru@gmail.com>
2022-08-01 00:08:11 -0700

    add 1 cpp

--- Coding/1/twoSum.cpp
+++ Coding/1/twoSum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    // Solution 1: Time efficiency: O(n^2)
+    vector<int> twoSum1(vector<int>& nums, int target) {
         vector<int> ret;
         for(int i = 0; i < nums.size();i++){
             for(int j = i+1; j < nums.size(); j++){
@@ -12,5 +13,39 @@ public:
             }
         }
         return ret;
+    }
+    
+    // Solution 2: Time Efficiency: O(nlogn);
+    vector<int> twoSum(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        int left = 0;
+        int right = nums.size()-1;
+        
+        while (true){
+            if (nums[left]+nums[right]==target){
+                vector<int> result;
+                result.emplace_back(left);
+                result.emplace_back(right);
+            } else if (nums[left]+nums[right]<target){
+                left += 1;
+            } else {
+                right -= 1;
+            }
+        }
+    }
+
+    // Solution 3: O(n); Using hashmap
+    vector<int> twoSum3(vector<int>& nums, int target) {
+        std::map<int,int> unique_numbers;
+        vector<int> result;
+        for(int i = 0; i < nums.size(); i++){
+            if (unique_numbers.find(target-nums[i])!=unique_numbers.end()){
+                result.emplace_back(i);
+                result.emplace_back(unique_numbers[target-nums[i]]);
+                break;
+            }
+            unique_numbers[nums[i]]=i;
+        }
+        return result;
     }
 };
