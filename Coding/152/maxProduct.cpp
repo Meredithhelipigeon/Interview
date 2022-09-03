180eb3b6feae8004307a22cc33200d07da4af34b
Meredithhelipigeon <meredithxinru@gmail.com>
2022-09-03 07:32:50 -0700

    add dp

--- Coding/152/maxProduct.cpp
+++ Coding/152/maxProduct.cpp
@@ -1,5 +1,7 @@
 // divide and conquer
-class Solution1 {
+// Time efficiency: O(nlogn)
+// Space efficiency: O(1)
+class DC_Solution {
 public:
     std::tuple<int, int> find_mami(vector<int>& vi, string order){
         int iteration = -1;
@@ -35,5 +37,46 @@ public:
     }
     int maxProduct(vector<int>& nums) {
         return divide_and_conquer(nums);
+    }
+};
+
+// Dynamic Programming
+// Time efficiency: O(n)
+// Space efficiency: O(n)
+class DP_Solution_1 {
+public:
+    int maxProduct(vector<int>& nums) {
+        int dp[nums.size()][2];
+        dp[0][0] = nums[0];
+        dp[0][1] = nums[0];
+        int ret = dp[0][0];
+        
+        for(int i = 1; i < nums.size(); i++){
+            dp[i][0] = min(nums[i],min(nums[i]*dp[i-1][0], nums[i]*dp[i-1][1]));
+            dp[i][1] = max(nums[i],max(nums[i]*dp[i-1][0], nums[i]*dp[i-1][1]));
+            ret = max(ret, max(dp[i][0],dp[i][1]));
+        }
+        
+        return ret;
+    }
+};
+
+// Time efficiency: O(n)
+// Space efficiency: O(1)
+class DP_Solution_2 {
+public:
+    int maxProduct(vector<int>& nums) {
+        int min_so_far = nums[0];
+        int max_so_far = nums[0];
+        int ret = nums[0];
+        
+        for(int i = 1; i < nums.size(); i++){
+            int last_min_so_far = min_so_far;
+            min_so_far = min(nums[i],min(nums[i]*min_so_far, nums[i]*max_so_far));
+            max_so_far = max(nums[i],max(nums[i]*last_min_so_far, nums[i]*max_so_far));
+            ret = max(max_so_far, ret);
+        }
+        
+        return ret;
     }
 };
