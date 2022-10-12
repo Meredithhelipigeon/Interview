113a4e78e1c8a449ad2413b70e96a2f7320834f0
Meredithhelipigeon <meredithxinru@gmail.com>
2022-10-12 00:01:12 -0400

    add increasing triplet

--- Coding/334/increasingTriplet.py
+++ Coding/334/increasingTriplet.py
@@ -1,4 +1,6 @@
 class Solution:
+    # Time efficiency: O(n)
+    # Space complexity: O(n)
     def increasingTriplet(self, nums: List[int]) -> bool:
         ifHasLeftSmaller = [False]*len(nums)
         ifHasRightBigger = [False]*len(nums)
@@ -19,5 +21,19 @@ class Solution:
         
         for i in range(len(nums)):
             if ifHasLeftSmaller[i] and ifHasRightBigger[i]:
+                return True
+        return False
+    
+    # Time efficiency: O(n)
+    # Space complexity: O(1)
+    def increasingTriplet2(self, nums: List[int]) -> bool:
+        smallest = second_smallest = float('inf')
+        
+        for i in range(len(nums)):
+            if nums[i] < smallest:
+                smallest = nums[i]
+            elif smallest < nums[i] < second_smallest:
+                second_smallest = nums[i]
+            elif nums[i]>second_smallest:
                 return True
         return False
